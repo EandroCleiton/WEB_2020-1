fbf181d5a191fe714a5b9a3f261cb604ab884fe2
Eandro <eandrocleiton@hotmail.com>
2020-10-22 15:30:22 -0300

    melhorando o formik de create e aplicando YUP

--- atividade11/Formik/src/components/Create.jsx
+++ atividade11/Formik/src/components/Create.jsx
@@ -1,67 +1,110 @@
-import React, { useState } from 'react'
+import React from 'react';
+import { Formik, Form, useField } from 'formik';
+import * as Yup from 'yup';
+
+const MyTextInput = ({ label, ...props }) => {
+    
+    const [field, meta] = useField(props);
+    return (
+        <div className="form-group">
+            <label htmlFor={props.id || props.name}>{label}</label>
+            <input {...field} {...props}
+                className={meta.touched ? (meta.error ? 'form-control is-invalid' : 'form-control is-valid') : 'form-control'}
+            />
+            {meta.touched && meta.error ? (
+                <div className="invalid-feedback">{meta.error}</div>
+            ) : null}
+        </div>
+    );
+};
+
 
 export default () => {
-
-    const [values, setValues] = useState({nomeDisciplina:'',curso:'',capacidade:''});
-
-    const handleChange = event => {
-        setValues(
-            {
-                ...values,
-                [event.target.name]: event.target.value
-            }
-        )
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        console.log(values.nomeDisciplina)
-        console.log(values.curso)
-        console.log(values.capacidade)
-    }
-
     return (
-        <div>
-            <h1>Criar Disciplina</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="nomeDisciplina">Nome da Disciplina: </label>
-                    <input
-                        id="nomeDisciplina"
-                        name="nomeDisciplina"
-                        type="text"
-                        onChange={handleChange}
-                        value={values.nomeDisciplina}
-                        className="form-control"
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="curso">Curso: </label>
-                    <input
-                        id="curso"
-                        name="curso"
-                        type="text"
-                        onChange={handleChange}
-                        value={values.curso}
-                        className="form-control"
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="capacidade">Capacidade: </label>
-                    <input
-                        id="capacidade"
-                        name="capacidade"
-                        type="capacidade"
-                        onChange={handleChange}
-                        value={values.capacidade}
-                        className="form-control"
-                    />
-                </div>
-                <div>
-                    <button className="btn btn-primary" type="submit">Submit</button>
-                </div>
-            </form>
-        </div>
-    )
+        <Formik
+            initialValues={
+                {
+                    nomeDisciplina: '',
+                    curso: '',
+                    capacidade: ''
+                }
+            }
+            validationSchema={Yup.object({
+                nomeDisciplina: Yup.string()
+                    .max(50, 'Must be 50 characters or less')
+                    .required('Required'),
+                curso: Yup.string()
+                    .max(35, 'Must be 35 characters or less')
+                    .required('Required'),
+                capacidade: Yup.string()
+                    .max(5, 'Must be 4 characters or less')
+                    .required('Required'),
+            })}
+            onSubmit={
+                (values, { setSubmitting }) => {
+                    setTimeout(
+                        () => {
+                            console.log(values.nomeDisciplina)
+                            console.log(values.curso)
+                            console.log(values.capacidade)
+                            setSubmitting(false)
+                        },
+                        200
+                    )
+                }
+            }
+        >
+            {
+                (props) => (
+                    <div className="card">
+                        <div className="card-header">Criar Disciplina</div>
+                        <div className="card-body">
+                            <Form>
 
-}
+                                <div className="form-row">
+                                    <div className="col-md-6">
+                                        <MyTextInput
+                                            label="Nome da disciplina"
+                                            name="nomeDisciplina"
+                                            id="nomeDisciplina"
+                                            type="text"
+                                            placeholder="Ex: Fundamentos da Programação"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="form-row">
+                                    <div className="col-md-6">
+                                        <MyTextInput
+                                            label="Curso"
+                                            name="curso"
+                                            id="curso"
+                                            type="text"
+                                            placeholder="Ex: Sistemas da Informação"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="form-row">
+                                    <div className="col-md-4">
+                                        <MyTextInput
+                                            label="Capacidade"
+                                            name="capacidade"
+                                            id="capacidade"
+                                            type="capacidade"
+                                            placeholder="Ex: 25"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="form-row">
+                                    <div className="col-md-12">
+                                        <button className="btn btn-primary" type="submit" disabled={props.isSubmitting ? true : false}>Submit</button>
+                                    </div>
+                                </div>
+                            </Form>
+                        </div>
+                    </div>
+                )
+            }
+
+        </Formik>
+    )
+        }
